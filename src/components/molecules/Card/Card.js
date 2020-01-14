18abfc5d64337570bcb2b1fba87b8e747280af07
Halina Balys <hbalys@gmail.com>
2020-01-14 23:24:54 +0100

    Card modiefied

--- src/components/molecules/Card/Card.js
+++ src/components/molecules/Card/Card.js
@@ -1,9 +1,34 @@
 import React from 'react';
+import styled from 'styled-components';
+import ButtonIcon from 'components/atoms/ButtonIcon/ButtonIcon';
+import Avatar1 from 'assets/avatars/1.jpg';
+
+const StyledWrapper = styled.div`
+  box-shadow: 0 3px 10px rgba(0, 0, 0, 0.3);
+  display: flex;
+`;
+const Avatar = styled.div`
+  height: 45px;
+  margin: 13px 40px;
+  width: 45px;
+  padding: 13px;
+  background-image: url(${Avatar1});
+  border-radius: 10px;
+`;
+
+const BreakLine = styled.div`
+  height: 45px;
+  width: 4px;
+  background-color: ${({ theme }) => theme.social};
+  margin-top: 13px;
+`;
 
 const Card = () => (
-  <div>
-    <p>ji</p>
-  </div>
+  <StyledWrapper>
+    <Avatar />
+    <BreakLine />
+    <ButtonIcon />
+  </StyledWrapper>
 );
 
 export default Card;
---
+++ src/components/molecules/Card/Card.stories.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Card from './Card';
+
+export default {
+  component: Card,
+  title: 'Card',
+};
+
+export const primaryCard = () => {
+  return <Card />;
+};
